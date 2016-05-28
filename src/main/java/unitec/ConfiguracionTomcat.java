90ad8116af5d1756e0681631ac4aec0723be4daa
campitos <campitos@campitos-Lenovo-G50-45>
2016-05-27 19:38:54 -0500

    ¿KKLJajsjajsñajlañd

---
+++ src/main/java/unitec/ConfiguracionTomcat.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author campitos
+ */
+@Configuration
+public class ConfiguracionTomcat implements
+        EmbeddedServletContainerCustomizer{
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer cesc) {
+        cesc.setPort(9000);
+    }
+    
+}
